49b7f4dc238cca32987af6fce101b7c774f5e823
Vijayakumar Mohan <95867013+Vijay-AWS-devops@users.noreply.github.com>
2025-07-03 23:23:08 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-# Stop the running container if any
-container_id=$(docker ps -q)
-
-if [ -n "$container_id" ]; then
-  docker rm -f "$container_id"
-  echo "Stopped and removed container: $container_id"
-else
-  echo "No running containers found."
-fi
+# Stop and remove the first running container (if any)
+container_id=$(docker ps -q | head -n 1)
+docker rm -f "$container_id"
