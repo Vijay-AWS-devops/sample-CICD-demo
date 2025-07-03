5679a32addda005295b36eaf7abcda2d4369086a
Vijayakumar Mohan <95867013+Vijay-AWS-devops@users.noreply.github.com>
2025-07-03 23:52:57 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Get the container ID of the latest running container
-LATEST_CONTAINER=$(docker ps -q --latest)
+# Stop and delete the running container
+container=$(docker ps -q | head -n 1)
+docker rm -f "$container"
 
-# Check if a container was found
-if [ -n "$LATEST_CONTAINER" ]; then
-    echo "Removing latest running container: $LATEST_CONTAINER"
-    sudo docker rm -f "$LATEST_CONTAINER"
-else
-    echo "No running containers found."
-fi
