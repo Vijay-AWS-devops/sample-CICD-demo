00eec12cf49b7976f7f282ad4da60a73e7553f2a
Vijayakumar Mohan <95867013+Vijay-AWS-devops@users.noreply.github.com>
2025-07-05 13:13:59 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -e
 
-# Stop and delete the running container
-echo "hi"
-
+# Stop and remove the running container
+container_id=$(docker ps -q)
+if [ -n "$container_id" ]; then
+  docker rm -f "$container_id"
+fi
